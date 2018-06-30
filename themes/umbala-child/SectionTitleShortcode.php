72bb1e8b318a154c77a79fd32ff85bcd725ebc48
Perfect Makanju <damiperfect@gmail.com>
2018-06-30 19:49:09 +0100

    Remove postal code field from checkout

--- themes/umbala-child/SectionTitleShortcode.php
+++ themes/umbala-child/SectionTitleShortcode.php
@@ -9,8 +9,8 @@ function asoSectionTitle($attrs) {
         'bottom' => ''
     ), $attrs );
 
-    $html = '<div class="section-title-container alus-section-title default aso-font no-bottom-margin">
-            <span class="before-title">'. $a['top'] .'</span>
+    $html = '<div class="section-title-container alus-section-title default no-bottom-margin">
+            <span class="before-title aso-font">'. $a['top'] .'</span>
             <div class="section-title-main">
                 <span class="font-900 " style="font-size:24px ">'. $a['middle'] . '</span>
             </div>
--- themes/umbala-child/functions.php
+++ themes/umbala-child/functions.php
@@ -8,8 +8,13 @@ function umbala_child_enqueue_styles() {
 
 wp_enqueue_style( 'umbala-style', get_template_directory_uri() . '/style.css', array('font-awesome') );
 wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('font-awesome') );
+}
 
 add_shortcode( 'aso_section_title', 'asoSectionTitle' );
 
+add_filter( 'woocommerce_checkout_fields' , 'asoRemoveBillingPostCode' );
+function asoRemoveBillingPostCode( $fields ) {
+  unset($fields['billing']['billing_postcode']);
+  return $fields;
 }
 
